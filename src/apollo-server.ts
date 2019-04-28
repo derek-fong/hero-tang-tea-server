bbe579f26eccd2e8fbabe6286c18203cd57ab32d
Derek Fong <derek-fong@users.noreply.github.com>
2019-04-28 20:29:17 +0800

    feat(app): setup Apollo Engine (#3)
    * Setup Apollo Engine.

--- src/apollo-server.ts
+++ src/apollo-server.ts
@@ -1,7 +1,13 @@
 import { ApolloServer } from 'apollo-server';
 
 import schema from './schema';
+import { environment } from './environment';
 
-const apolloServer = new ApolloServer({ schema });
+const apolloServer = new ApolloServer({
+  schema,
+  engine: { apiKey: environment.apollo.engine.apiKey },
+  introspection: environment.apollo.introspection,
+  playground: environment.apollo.playground
+});
 
 export default apolloServer;
--- src/environment.ts
+++ src/environment.ts
@@ -1,9 +1,23 @@
 const defaultPort = 4000;
 
 export interface Environment {
+  apollo: {
+    engine: {
+      apiKey: string;
+    };
+    introspection: boolean;
+    playground: boolean;
+  };
   port: number | string;
 }
 
 export const environment: Environment = {
+  apollo: {
+    engine: {
+      apiKey: process.env.APOLLO_ENGINE_API_KEY as string
+    },
+    introspection: process.env.APOLLO_INTROSPECTION === 'true',
+    playground: process.env.APOLLO_PLAYGROUND === 'true'
+  },
   port: process.env.PORT || defaultPort
 };
