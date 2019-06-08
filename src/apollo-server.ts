c6eb033ae260e75d911dfba479f9807c08759b64
Derek Fong <derek-fong@users.noreply.github.com>
2019-06-08 17:13:05 +0800

    feat(app): allow CORS (#5)
    * Allow CORS.

--- src/apollo-server.ts
+++ src/apollo-server.ts
@@ -7,6 +7,10 @@ const { schema } = AppModule;
 
 export const apolloServer = new ApolloServer({
   schema,
+  cors: {
+    credentials: true,
+    origin: '*'
+  },
   engine: { apiKey: environment.apollo.engine.apiKey },
   introspection: environment.apollo.introspection,
   playground: environment.apollo.playground
